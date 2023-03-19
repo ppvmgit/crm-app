b3ee860ccb5a10fa0b621633830f5562e1f9403d
ppvmgit <prasanna.technology@gmail.com>
2023-03-19 08:02:17 -0400

    Lead feature in Progress (#1)
    * New html created for insert and update

--- controller/lead.py
+++ controller/lead.py
@@ -1,1 +1,15 @@
-lead.py
+#Lead controller Implementation
+
+#create Lead
+
+def create_lead():
+	print 'Lead had been created'
+
+#update Lead
+def update_lead():
+	print 'Lead had been updated'
+
+
+#list Lead
+def update_get():
+	print 'List all the Leads'
---
+++ model/lead.py
@@ -0,0 +1,13 @@
+#New Model Created
+
+def insert_lead():
+	print 'Lead had been created in db'
+
+#update Lead
+def update_all_lead():
+	print 'Lead had been updated in db'
+
+
+#list Lead
+def getall_lead():
+	print 'List all the Leads from db'
